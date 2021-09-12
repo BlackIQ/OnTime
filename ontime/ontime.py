3e1c9ed17c951e0332127aa91cadf728922bce7f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-09-12 15:19:54 +0430

    Add ontime python file

---
+++ ontime/ontime.py
@@ -0,0 +1,28 @@
+import notifypy
+import time
+import sys
+
+notification = notifypy.Notify()
+
+usertime = sys.argv[1]
+
+def done(usert):
+	notification.title = f"Time is {usert}"
+	notification.message = f"You set the alarm for {usert}"
+	notification.audio = "files/alarm.wav"
+	notification.icon = "files/icon.png"
+
+	notification.send()
+
+while True:
+	now = time.strftime("%H:%M:%S", time.localtime())
+	
+	if now == usertime:
+		break
+	else:
+		time.sleep(1)
+		pass
+		
+while True:
+	done(usertime)
+	time.sleep(10)
