2187089a0f007cc34116826262d7ae0320315c51
shabane <m.mohamadshabane@gmail.com>
2021-09-14 14:47:11 +0430

    setup writed and work well

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/bash
+
+pip3 install notify-py
+
+sudo rm -rf ~/.local/bin/ontime /opt/ontime
+mv ./ontime /opt/
+ln -s /opt/ontime/ontime.py ~/.local/bin/ontime
+
+echo "installation done, enjoy"
